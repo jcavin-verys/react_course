d4e47ac5c2c2e03c5350d321ac312e1d7b31938b
Joshua Cavin <josh.cavin@verys.com>
2016-06-28 10:51:28 -0700

    video 3

--- app/index.js
+++ app/index.js
@@ -1,2 +1,15 @@
-var app = document.getElementByID('app');
-app.innerHTML = "Hello";
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var HelloWorld = React.createClass({
+  render: function () {
+    return (
+      <div>Hello { this.props.name }</div>
+    )
+  }
+});
+
+ReactDOM.render(
+  <HelloWorld name="Tyler" />,
+  document.getElementById('app')
+);
