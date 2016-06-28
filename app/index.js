745dd5c77b4485ad014c45aae6b8d29f16afc838
Joshua Cavin <josh.cavin@verys.com>
2016-06-28 15:14:50 -0700

    index

--- app/index.js
+++ app/index.js
@@ -1,15 +1,63 @@
+var USER_DATA = {
+  name: 'Tyler Mac',
+  username: 'tylermac',
+  image: 'http://www.planwallpaper.com/static/images/cool_cat.jpg'
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
+  render: function () {
+    return <img src={this.props.imageurl} height="100" />
+  }
+});
+
+var Link = React.createClass({
   render: function () {
     return (
-      <div>Hello { this.props.name }</div>
+      <span>
+        {this.props.children}
+      </span>
+    )
+  }
+});
+
+var ProfileLink = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <Link href={this.props.username}>
+          {this.props.username}
+        </Link>
+      </div>
+    )
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function () {
+    return (
+      <div>
+        {this.props.name}
+      </div>
+    )
+  }
+});
+
+var Avatar = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <ProfilePic imageurl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
     )
   }
 });
 
 ReactDOM.render(
-  <HelloWorld name="Tyler" />,
+  <Avatar user={USER_DATA} />,
   document.getElementById('app')
 );
