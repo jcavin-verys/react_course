4e7a066d4a99d9583c195ccb3b9fba33aa35d8a4
Joshua Cavin <josh.cavin@verys.com>
2016-06-29 11:03:04 -0700

    routes tutorial

--- app/index.js
+++ app/index.js
@@ -1,63 +1,8 @@
-var USER_DATA = {
-  name: 'Tyler Mac',
-  username: 'tylermac',
-  image: 'http://www.planwallpaper.com/static/images/cool_cat.jpg'
-}
-
 var React = require('react');
 var ReactDOM = require('react-dom');
-
-var ProfilePic = React.createClass({
-  render: function () {
-    return <img src={this.props.imageurl} height="100" />
-  }
-});
-
-var Link = React.createClass({
-  render: function () {
-    return (
-      <span>
-        {this.props.children}
-      </span>
-    )
-  }
-});
-
-var ProfileLink = React.createClass({
-  render: function () {
-    return (
-      <div>
-        <Link href={this.props.username}>
-          {this.props.username}
-        </Link>
-      </div>
-    )
-  }
-});
-
-var ProfileName = React.createClass({
-  render: function () {
-    return (
-      <div>
-        {this.props.name}
-      </div>
-    )
-  }
-});
-
-var Avatar = React.createClass({
-  render: function () {
-    return (
-      <div>
-        <ProfilePic imageurl={this.props.user.image} />
-        <ProfileName name={this.props.user.name} />
-        <ProfileLink username={this.props.user.username} />
-      </div>
-    )
-  }
-});
+var routes = require('./config/routes')
 
 ReactDOM.render(
-  <Avatar user={USER_DATA} />,
+  routes,
   document.getElementById('app')
 );
